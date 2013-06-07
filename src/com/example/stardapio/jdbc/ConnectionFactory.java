226f09f4e3dfdfa528972661fdb683fee545275a
vinicius <viniciusdsaraiva@gmail.com>
2013-06-07 14:05:16 -0300

    WAR

--- src/com/example/stardapio/jdbc/ConnectionFactory.java
+++ src/com/example/stardapio/jdbc/ConnectionFactory.java
@@ -9,7 +9,7 @@ public class ConnectionFactory {
 		try {
 			DriverManager.registerDriver(new com.mysql.jdbc.Driver()); //Essa linha foi a diferença
 			return DriverManager.getConnection(
-					"jdbc:mysql://localhost/stardapio", "root", "123456");
+					"jdbc:mysql://localhost/stardapio", "root", "stardapio2013");
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
