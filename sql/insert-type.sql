6f3b5ed81a94cd40e7fb882da972fb19b8894d80
vinids86 <viniciusdsaraiva@gmail.com>
2013-06-18 00:07:56 -0300

    America

---
+++ sql/insert-type.sql
@@ -0,0 +1,16 @@
+use stardapio;
+
+insert into type (type) values ('Saladas');
+insert into type (type) values ('Grelahdos');
+insert into type (type) values ('Pasta e Cia');
+insert into type (type) values ('Peixes');
+insert into type (type) values ('Sandwiches');
+insert into type (type) values ('Burger');
+insert into type (type) values ('Special');
+insert into type (type) values ('Monte seu Prato');
+insert into type (type) values ('Menu Kids');
+insert into type (type) values ('Acompanhamentos');
+insert into type (type) values ('Bebidas');
+insert into type (type) values ('Sobremesas');
+insert into type (type) values ('Winter Soup');
+insert into type (type) values ('Sobremesa de inverno');
